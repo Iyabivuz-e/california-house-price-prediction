385b90ac8387ff321c2d275eeabb4ca941c3b5ea
Dieudonne <iyabivuzed@gmail.com>
2025-02-28 23:32:59 +0100

    updated the app.py

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ scalar = pickle.load(open('scaling.pkl', 'rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/api/predict', methods=['POST']):
-    def predict_api():
-        data = request.json['data']
-        print(f'my data: {data}')
-        #Since the data is in json format (dictionary), we will have to reshape it in a list format and np array
-        data_in_array = np.array(list(data.values())).reshape(1, -1)
-        print(f'my data in array format: {data_in_array}')
-        new_data = scalar.transform(data_in_array)
-        prediction = model.predict(new_data)
-        print(f'prediction is: {prediction[0]}')
-        retrun jsonify(prediction[0])
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(f'my data: {data}')
+    #Since the data is in json format (dictionary), we will have to reshape it in a list format and np array
+    data_in_array = np.array(list(data.values())).reshape(1, -1)
+    print(f'my data in array format: {data_in_array}')
+    new_data = scalar.transform(data_in_array)
+    prediction = model.predict(new_data)
+    print(f'prediction is: {prediction[0]}')
+    return jsonify(prediction[0])
         
         
 if __name__ == '__main__':
